6b9114e0c0668f35c05314ceaef4a4cc1b585486
Vishwa23 <vchaganti2@student.gsu.edu>
2017-04-18 15:10:58 -0400

    changed addButtonAction to pass arguments to JoinGameMenu instead of creating a new JoinGameMenu Scene

--- src/com/dd/controller_util/controller/AddServerController.java
+++ src/com/dd/controller_util/controller/AddServerController.java
@@ -1,9 +1,48 @@
 package com.dd.controller_util.controller;
 
+import com.dd.DandD;
 import com.dd.controller_util.ControllerArgumentPackage;
 import com.dd.controller_util.GameSceneController;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class AddServerController extends GameSceneController{
+	@FXML
+	Label serverName;
+	@FXML
+	Label ipAddress;
+	@FXML
+	TextField serverNameField;
+	@FXML
+	TextField ipAddressField;
+	@FXML
+	Button addButton;
+	@FXML
+	Label errorLabel;
+	@FXML
+	private void addButtonAction(ActionEvent event){
+		if(checkFields()){
+			ControllerArgumentPackage serverGame= new ControllerArgumentPackage();
+			serverGame.setArgument("ServerName",serverNameField.getText());
+			serverGame.setArgument("IPAddress",  ipAddressField.getText());
+			DandD.setActiveGameScene("JoinGameScene", serverGame);
+		}
+	}
+	private boolean checkFields(){
+		if(serverNameField.getText()==""){
+			errorLabel.setText("Please enter the server name");
+			return false;
+		}
+		if(ipAddressField.getText()==""){
+			errorLabel.setText("Please enter the ip address");
+			return false;
+		}
+		return true;
+	}
     @Override
     public void setup(ControllerArgumentPackage args){
 
